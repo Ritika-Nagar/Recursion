58bde4854669803b1ee523268557a305ead4a385
Ritika Nagar <87242161+Ritika-Nagar@users.noreply.github.com>
2022-03-04 03:35:59 +0530

    Add files via upload

---
+++ pallindrome.java
@@ -0,0 +1,13 @@
+package recursion;
+
+public class pallindrome {
+
+	public static void checkPallindrome() {
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
